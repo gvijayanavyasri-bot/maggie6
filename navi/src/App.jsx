688facbe9e84a67969705b06bb88d05711817a71
chmeghana81-dot <chmeghana81@gmail.com>
2026-07-24 15:44:24 +0530

    added line

--- navi/src/App.jsx
+++ navi/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <h2>my name is navi</h2>
       <h4>hello javascript</h4>
       <p>A SMILE IS A CURVE THAT SETS EVERYTHING STRAIGHT</p>
+      <h2>the mainly using this github profile</h2>
     </div>
   );
 };
