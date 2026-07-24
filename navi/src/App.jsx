66613f0619a5fd09684657f30fc5b9388e139a2a
subba2002 <ramsubbarao0@example.com>
2026-07-24 16:20:36 +0530

    git push

--- navi/src/App.jsx
+++ navi/src/App.jsx
@@ -8,12 +8,7 @@ const App = () => {
       <p>the javascript using this language web applications and using dainamic language</p>
       <p>A SMILE IS A CURVE THAT SETS EVERYTHING STRAIGHT</p>
       <h2>i amthe react</h2>
-
-      <img src="https://unsplash.com/photos/dog-running-on-beach-during-daytime-yihlaRCCvd4?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink" alt=""/>
-
       <h2>the mainly using this github profile</h2>
- 688facbe9e84a67969705b06bb88d05711817a71
-origin/main
     </div>
   );
 };
