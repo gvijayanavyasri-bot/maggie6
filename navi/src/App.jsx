11bd6a62c018bfb4132d1fc9bd8a8fa20df8a620
subba2002 <ramsubbarao0@example.com>
2026-07-24 15:46:54 +0530

    added image

--- navi/src/App.jsx
+++ navi/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <h2>my name is navi</h2>
       <h4>hello javascript</h4>
       <p>A SMILE IS A CURVE THAT SETS EVERYTHING STRAIGHT</p>
+      <img src="https://unsplash.com/photos/dog-running-on-beach-during-daytime-yihlaRCCvd4?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink" alt=""/>
     </div>
   );
 };
