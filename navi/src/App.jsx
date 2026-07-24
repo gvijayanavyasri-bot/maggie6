561486acd9e3c79bf2ea4d2df671e59f66502595
subba2002 <ramsubbarao0@example.com>
2026-07-24 16:12:05 +0530

    added line

--- navi/src/App.jsx
+++ navi/src/App.jsx
@@ -6,11 +6,7 @@ const App = () => {
       <h2>my name is navi</h2>
       <h4>hello javascript</h4>
       <p>A SMILE IS A CURVE THAT SETS EVERYTHING STRAIGHT</p>
-<<<<<<< HEAD
-      <img src="https://unsplash.com/photos/dog-running-on-beach-during-daytime-yihlaRCCvd4?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink" alt=""/>
-=======
-      <h2>the mainly using this github profile</h2>
->>>>>>> 688facbe9e84a67969705b06bb88d05711817a71
+    
     </div>
   );
 };
