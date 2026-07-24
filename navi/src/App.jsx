efa2f1ededd7323d6ca5eff78b48fa8ad2bc24be
navya <gvijayanavyasri@gmail.com>
2026-07-24 15:34:01 +0530

    added code

---
+++ navi/src/App.jsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2>my name is navi</h2>
+      <h4>hello javascript</h4>
+    </div>
+  );
+};
+
+export default memo(App);
